45ae6f6de6ef0f5cfbccaebab99c066e73ae9eae
Hamidreza Baniasadi <hamidreza.neo@gmail.com>
2020-10-15 10:04:36 +0330

    svae user data to registry

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 import sys
-import threading
 import loading
+import threading
+from winreg import *
 from website import Website
 
-user = Website("2981485792")
+# init registry
+keyVal = 'Software\\Goshad'
+try:
+    key = OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS)
+except:
+    key = CreateKey(HKEY_CURRENT_USER, keyVal)
+
+# Login
+try:
+	nationalId = QueryValueEx(key, "nationalId")[0]
+except:
+	nationalId = input("Enter your national id: ")
+
+user = Website(nationalId)
 
 loading_process = threading.Thread(target=user.login)
 loading_process.start()
 login = loading.simple(loading_process, "Login... ")
 loading_process.join()
 
-loading_process = threading.Thread(target=user.openClass, args=("شیمی",))
-loading_process.start()
-login = loading.simple(loading_process, "Getting Lesson URL... ")
-loading_process.join()
+# try Login again if failed
+while user.status[0] == 401:
+	print(user.status[1])
+	nationalId = input("Enter your national id: ")
+	user = Website(nationalId)
+
+	loading_process = threading.Thread(target=user.login)
+	loading_process.start()
+	login = loading.simple(loading_process, "Login... ")
+	loading_process.join()
+
+# Write login detail to registry
+SetValueEx(key, "nationalId", 0, REG_SZ, nationalId)
+CloseKey(key)
+
+# loading_process = threading.Thread(target=user.openClass, args=("شیمی",))
+# loading_process.start()
+# login = loading.simple(loading_process, "Getting Lesson URL... ")
+# loading_process.join()
--- website.py
+++ website.py
@@ -8,6 +8,8 @@ from bs4 import BeautifulSoup
 from urllib.parse import unquote
 
 class Website:
+	page = ""
+	status = [0, '']
 	session = requests.Session()
 
 	def __init__(self, identity):
@@ -35,48 +37,58 @@ class Website:
 
 				if "logintoken" in response.text:
 					os.system('cls')
-					print("\nLogin faild, try again.")
-					continue
+					msg = "Login faild, try again."
+					self.status = [401, msg]
+					return
 				else:
 					self.page = BeautifulSoup(response.content, 'html.parser')
 					self.name = self.page.find_all("h1")[0].text
 					break
 			except:
 				os.system('cls')
-				print("\nProblem with login, try again.")
+				print("Problem with login, try again.")
 				continue
 
 	def openClass(self, lesson):
-		elems = self.page.find_all("div", class_="media-body")
-		found = False
-		# do all the redirects 😥
-		for elem in elems:
-			name = elem.find("h4").find("a").text
-			url = elem.find("h4").find("a")["href"]
-			if lesson in name:
-				found = True
-				response = self.session.get(url)
-				soup = BeautifulSoup(response.content, 'html.parser')
+		try:
+			if "logintoken" in str(self.page) or self.page == "":
+				print("Session faild, try to login.")
+				self.login()
 
-				url = soup.find_all("div", class_="activityinstance")[1].find("a")["href"]
-				response = self.session.get(url)
-				soup = BeautifulSoup(response.content, 'html.parser')
+			elems = self.page.find_all("div", class_="media-body")
+			found = False
+			# do all the redirects 😥
+			for elem in elems:
+				name = elem.find("h4").find("a").text
+				url = elem.find("h4").find("a")["href"]
+				if lesson in name:
+					found = True
+					response = self.session.get(url)
+					soup = BeautifulSoup(response.content, 'html.parser')
 
-				onclick = soup.find_all("input")[3]["onclick"]
-				url = re.search("(?P<url>https?://[^\s]+)", onclick).group("url").strip("',")
+					url = soup.find_all("div", class_="activityinstance")[1].find("a")["href"]
+					response = self.session.get(url)
+					soup = BeautifulSoup(response.content, 'html.parser')
 
-				# Adobe Connect URL
-				response = self.session.get(url)
-				soup = BeautifulSoup(response.content, 'html.parser')
-				script = soup.find_all("script")[5]
-				script_regex = re.search("https?[^\s]+;", str(script)).group().strip("';")+";"
-				adobe_url = "connectpro:" + script_regex
+					onclick = soup.find_all("input")[3]["onclick"]
+					url = re.search("(?P<url>https?://[^\s]+)", onclick).group("url").strip("',")
 
-				webbrowser.open(unquote(adobe_url))
-				break
+					# Adobe Connect URL
+					response = self.session.get(url)
+					soup = BeautifulSoup(response.content, 'html.parser')
+					script = soup.find_all("script")[5]
+					script_regex = re.search("https?[^\s]+;", str(script)).group().strip("';")+";"
+					adobe_url = "connectpro:" + script_regex
 
-		if not found: 
-			print("Can't find the lesson")
-			return False
+					webbrowser.open(unquote(adobe_url))
+					break
 
-		return True
+			if not found: 
+				print("Can't find the lesson")
+				return False
+
+			return True
+
+		except Exception as e:
+			os.system('cls')
+			print("Error: " + e)
